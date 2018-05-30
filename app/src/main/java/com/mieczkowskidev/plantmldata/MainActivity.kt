6c496aa43292bf0498b69e5f8c82280edebd64d0
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-05-30 18:18:41 +0200

    added action button

--- app/src/main/java/com/mieczkowskidev/plantmldata/MainActivity.kt
+++ app/src/main/java/com/mieczkowskidev/plantmldata/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.mieczkowskidev.plantmldata
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_button.setOnClickListener { _ ->
+            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show() }
     }
 }
