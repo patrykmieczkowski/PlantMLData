10bd327c3e9e171f0f8d29143928fcf8aa1c6e4c
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-05-31 13:05:48 +0200

    Capturing photo from camera

--- app/src/main/java/com/mieczkowskidev/plantmldata/MainActivity.kt
+++ app/src/main/java/com/mieczkowskidev/plantmldata/MainActivity.kt
@@ -1,17 +1,105 @@
 package com.mieczkowskidev.plantmldata
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
+import android.util.Log
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetectorOptions
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAKE_PHOTO_REQUEST = 101
+    private var mCurrentPhotoPath: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        main_button.setOnClickListener { _ ->
-//            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show() }
+        main_button.setOnClickListener { _ -> checkPermissions() }
+        firebaseSettings()
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == TAKE_PHOTO_REQUEST) {
+            val imageUri = data?.data
+            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun checkPermissions() {
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                        launchCamera()
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(permission: PermissionRequest?,
+                                                                    token: PermissionToken?) {
+                        AlertDialog.Builder(this@MainActivity)
+                                .setTitle("Give permissions")
+                                .setMessage("Write storage")
+                                .setNegativeButton(android.R.string.cancel,
+                                        { dialog, _ ->
+                                            dialog.dismiss()
+                                            token?.cancelPermissionRequest()
+                                        })
+                                .setPositiveButton(android.R.string.ok,
+                                        { dialog, _ ->
+                                            dialog.dismiss()
+                                            token?.continuePermissionRequest()
+                                        })
+                                .setOnDismissListener({ token?.cancelPermissionRequest() })
+                                .show()
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                        Log.e("Main", "No permissions - denied")
+                    }
+                })
+                .check()
+
+    }
+
+    private fun launchCamera() {
+        val values = ContentValues(1)
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+        val fileUri = contentResolver
+                .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        values)
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            mCurrentPhotoPath = fileUri.toString()
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            startActivityForResult(intent, TAKE_PHOTO_REQUEST)
+        }
+    }
+
+    private fun firebaseSettings() {
+
+        // configure image labeler, by defailt returns 10 labels for image - to change it use the code below
+        var fbOptions = FirebaseVisionLabelDetectorOptions.Builder()
+                .setConfidenceThreshold(0.8f)
+                .build()
     }
 }
